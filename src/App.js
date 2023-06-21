52035a8bd5af51a3f073aa9e860995a85b99a635
Adil Heybetov <heybetov1998@gmail.com>
2023-06-22 01:47:08 +0400

    added react router inside project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import styles from "./App.module.scss";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Products from "./pages/products";
+import ProductDetails from "./pages/productDetails";
+import Cart from "./pages/cart";
+import Header from "./components/layout/Header";
+import NotFound from "./pages/notFound";
 
 function App() {
-    return <div className={styles.app}></div>;
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/products">
+                    <Route index element={<Products />} />
+                    <Route path=":id" element={<ProductDetails />} />
+                </Route>
+                <Route path="/cart" element={<Cart />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
