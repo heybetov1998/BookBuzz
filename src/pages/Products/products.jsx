e0495fc57d688c6d7f13064c1e471b33c0680f92
Adil Heybetov <heybetov1998@gmail.com>
2023-06-22 16:27:52 +0400

    added content

--- src/pages/Products/products.jsx
+++ src/pages/Products/products.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import ProductCard from "../../components/ui/ProductCard/ProductCard";
+import styles from "./products.module.scss";
 
 const Products = () => {
-    return <div>Products</div>;
+    return (
+        <section className={styles.productsPage}>
+            <div className="container">
+                <h2 className={styles.sectionName}>Browse All Books</h2>
+                <div className="row">
+                    <div className="col-md-3">
+                        <ProductCard />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Products;
