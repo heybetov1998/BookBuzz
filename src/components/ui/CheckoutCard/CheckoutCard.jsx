65caea3b4b1e2a73de0fc2657995013d17e8508e
Adil Heybetov <heybetov1998@gmail.com>
2023-06-23 15:42:03 +0400

    CheckoutCard component is ready

--- src/components/ui/CheckoutCard/CheckoutCard.jsx
+++ src/components/ui/CheckoutCard/CheckoutCard.jsx
@@ -1,3 +1,4 @@
+import CheckoutItem from "../CheckoutItem/CheckoutItem";
 import styles from "./CheckoutCard.module.scss";
 
 const CheckoutCard = () => {
@@ -8,7 +9,28 @@ const CheckoutCard = () => {
             </div>
             <div className={styles.body}>
                 <div className={styles.box}>
-                    
+                    <CheckoutItem
+                        boldTitle
+                        hasQuantity
+                        title="Book title"
+                        price={10}
+                    />
+                    <CheckoutItem
+                        boldTitle
+                        hasQuantity
+                        title="Book title"
+                        price={10}
+                    />
+                </div>
+                <div className={styles.box}>
+                    <CheckoutItem title="Subtotal" price={22} />
+                    <CheckoutItem title="Shipping" price={3.26} />
+                    <CheckoutItem
+                        boldTitle
+                        boldPrice
+                        title="Total"
+                        price={25.26}
+                    />
                 </div>
             </div>
             <div className={styles.footer}></div>
