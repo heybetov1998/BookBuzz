9b4fa00d1163fc1c973d49ad279915c0d261aba8
Adil Heybetov <heybetov1998@gmail.com>
2023-06-23 15:42:26 +0400

    CheckoutItem component is ready

--- src/components/ui/CheckoutItem/CheckoutItem.jsx
+++ src/components/ui/CheckoutItem/CheckoutItem.jsx
@@ -1,13 +1,26 @@
 import styles from "./CheckoutItem.module.scss";
 
-const CheckoutItem = () => {
+const CheckoutItem = ({
+    boldTitle = false,
+    boldPrice = false,
+    hasQuantity = false,
+    title = "Title",
+    price = 0,
+    quantity = 1,
+}) => {
     return (
         <div className={styles.checkoutItem}>
             <div className={styles.top}>
-                <div className={styles.title}></div>
-                <div className={styles.price}></div>
+                <h3 className={boldTitle ? styles.boldTitle : styles.title}>
+                    {title}
+                </h3>
+                <span className={boldPrice ? styles.boldPrice : styles.price}>
+                    ${price.toFixed(2)}
+                </span>
             </div>
-            <p className={styles.quantity}></p>
+            {hasQuantity && (
+                <p className={styles.quantity}>Quantity: {quantity}</p>
+            )}
         </div>
     );
 };
