626734c470432989aedb77193ed6d4e24175dda9
Adil Heybetov <heybetov1998@gmail.com>
2023-06-23 17:46:48 +0400

    cart page is ready

--- src/pages/Cart/cart.jsx
+++ src/pages/Cart/cart.jsx
@@ -1,8 +1,20 @@
 import CartItem from "../../components/ui/CartItem/CartItem";
 import CheckoutCard from "../../components/ui/CheckoutCard/CheckoutCard";
 import styles from "./cart.module.scss";
+// import withReactContent from "sweetalert2-react-content";
+// import Swal from "sweetalert2";
+
+// const MySwal = withReactContent(Swal);
 
 const Cart = () => {
+    // MySwal.fire({
+    //     icon: "success",
+    //     title: "Success!",
+    //     text: "Order was placed successfully and will be delivered in 3-5 business days",
+    //     confirmButtonColor: "#030303",
+    //     confirmButtonText: "Dismiss",
+    // });
+
     return (
         <section className={styles.cartPage}>
             <div className="container">
