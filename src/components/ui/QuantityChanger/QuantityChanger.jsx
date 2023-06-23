1d0d3ce9e1709d13b60d0e41161705245badf533
Adil Heybetov <heybetov1998@gmail.com>
2023-06-23 12:55:28 +0400

    Quantity changer is ready

---
+++ src/components/ui/QuantityChanger/QuantityChanger.jsx
@@ -0,0 +1,18 @@
+import styles from "./QuantityChanger.module.scss";
+import { FaPlus, FaMinus } from "react-icons/fa6";
+
+const QuantityChanger = ({ className }) => {
+    return (
+        <div className={`${styles.quantityChanger} ${className ?? ""}`}>
+            <span className={styles.button}>
+                <FaMinus />
+            </span>
+            <span className={styles.quantity}>2</span>
+            <span className={styles.button}>
+                <FaPlus />
+            </span>
+        </div>
+    );
+};
+
+export default QuantityChanger;
