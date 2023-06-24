f7c12b26b337229a41faf9dfdee307dab44691ab
Adil Heybetov <heybetov1998@gmail.com>
2023-06-24 14:09:12 +0400

    productDetails page is ready desktop version

--- src/pages/ProductDetails/productDetails.jsx
+++ src/pages/ProductDetails/productDetails.jsx
@@ -1,7 +1,11 @@
 import Info from "../../components/ui/Info/Info";
 import Tag from "../../components/ui/Tag/Tag";
+import QuantityChanger from "../../components/ui/QuantityChanger/QuantityChanger";
+import Button from "../../components/ui/Button/Button";
 import styles from "./productDetails.module.scss";
 // import { useParams } from "react-router-dom";
+import dog from "../../assets/images/dog_pp.jpg";
+import SquareImage from "../../components/ui/SquareImage/SquareImage";
 
 const ProductDetails = () => {
     // const { id } = useParams();
@@ -11,12 +15,26 @@ const ProductDetails = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="gallery">
-                            <div className="main"></div>
-                            <div className="others"></div>
+                        <div className={styles.gallery}>
+                            <div className={styles.main}>
+                                <SquareImage />
+                            </div>
+                            <div className={styles.others}>
+                                <div className="row">
+                                    <div className="col-md-4 mb-4">
+                                        <SquareImage />
+                                    </div>
+                                    <div className="col-md-4 mb-4">
+                                        <SquareImage />
+                                    </div>
+                                    <div className="col-md-4 mb-4">
+                                        <SquareImage />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-md-6">
+                    <div className={`col-md-6 ${styles.product}`}>
                         <h2 className={styles.sectionName}>Product Name</h2>
                         <p className="overview">
                             Lorem ipsum, dolor sit amet consectetur adipisicing
@@ -37,6 +55,16 @@ const ProductDetails = () => {
                             <Info />
                             <Info />
                         </div>
+                        <div className={styles.quantityPrice}>
+                            <div className={styles.quantity}>
+                                <QuantityChanger />
+                                <p className={styles.inStock}>In Stock: 5</p>
+                            </div>
+                            <p className={styles.price}>
+                                Price: <span>$14.00</span>
+                            </p>
+                        </div>
+                        <Button className="mt-4">Add to cart</Button>
                     </div>
                 </div>
             </div>
