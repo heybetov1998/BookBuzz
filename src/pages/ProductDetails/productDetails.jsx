476d7cbf299280d2bb1b96250c99b1ffe62ef04f
Adil Heybetov <heybetov1998@gmail.com>
2023-06-24 11:50:48 +0400

    moved productDetails into folder

---
+++ src/pages/ProductDetails/productDetails.jsx
@@ -0,0 +1,10 @@
+import styles from "./productDetails.module.scss";
+// import { useParams } from "react-router-dom";
+
+const ProductDetails = () => {
+    // const { id } = useParams();
+
+    return <section className={styles.productDetailsPage}></section>;
+};
+
+export default ProductDetails;
